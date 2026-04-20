14b653d38dc128c4461a74911f3987b35fd3b5b8
KI1900 <rifkihs26@gmail.com>
2026-04-20 14:52:10 +0700

    sudah selesai

--- main.java
+++ main.java
@@ -1,15 +1,17 @@
 
-import base.Produk;
+import tes.Produk;
 
 public class main {
     public static void main(String[] args) {
-        Produk P = new Produk("Indomie Goreng", 3500, 100, "Makanan");
+        Produk P = new Produk("Indomie Goreng", 3500, 100);
     
-        System.out.println(P.stok);
-        // System.out.println(P.harga);
-        // System.out.println(P.Kategori);
-        // System.out.println(P.nama);
-
-        P.tampilkanInfo();
+        System.out.println("Tampilkan Harga: " + P.getHarga());
+        System.out.println("Tampilkan Stok: " + P.getStok());
+        System.out.println("Tampilkan Nama: " + P.getNama());
+        P.setHarga(1000);
+        P.setStok(150);
+        System.out.println(P.getHarga());
+        System.out.println(P.getStok());
+        System.out.println(P.getNama());
     }
 }
--- tes/Produk.java
+++ tes/Produk.java
@@ -1,14 +1,12 @@
-package base;   
+package tes;   
    
 public class Produk {
         private String nama;
-        String Kategori;
         protected double harga;
         public int stok;
 
-        public Produk(String nama, String Kategori, double harga, int stok) {
+        public Produk(String nama, double harga, int stok) {
             this.nama = nama;
-            this.Kategori = Kategori;
             this.harga = harga;
             this.stok = stok;
         }
@@ -25,11 +23,13 @@ public class Produk {
         public void setStok(int stok) {
             this.stok = stok;
         }
+        public void setHarga(double harga) {
+            this.harga = harga;
+        }
 
 
         public void tampilkanInfo() {
             System.out.println("Nama Produk: " + nama);
-            System.out.println("Kategori: " + Kategori);
             System.out.println("Harga: " + harga);
             System.out.println("Stok: " + stok);
             System.out.println("-----------------------------");
